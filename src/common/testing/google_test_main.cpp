35668da69870fd980b69d1f4b2afc742404fbb8c
africamonkey <marses@vip.qq.com>
2023-04-16 11:58:50 +0800

    add error handler.

--- src/common/testing/google_test_main.cpp
+++ src/common/testing/google_test_main.cpp
@@ -1,6 +1,8 @@
+#include "glog/logging.h"
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
+  google::InstallFailureSignalHandler();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
