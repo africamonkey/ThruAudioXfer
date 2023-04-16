ec0ccf83d4a20d5bf7e36cf336de4643957f2a9f
africamonkey <marses@vip.qq.com>
2023-04-16 20:04:04 +0800

    Revert "init google logging."
    This reverts commit 1c9dc9606a2be645a9f8c8b10c9f93935336bdb7.

--- src/common/testing/google_test_main.cpp
+++ src/common/testing/google_test_main.cpp
@@ -2,7 +2,6 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
-  google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
