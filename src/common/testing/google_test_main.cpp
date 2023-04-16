1c9dc9606a2be645a9f8c8b10c9f93935336bdb7
africamonkey <marses@vip.qq.com>
2023-04-16 19:58:26 +0800

    init google logging.

--- src/common/testing/google_test_main.cpp
+++ src/common/testing/google_test_main.cpp
@@ -2,6 +2,7 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
