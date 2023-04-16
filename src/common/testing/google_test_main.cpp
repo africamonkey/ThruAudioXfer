8eb32c15b54e88d266d2519a94fb9613bc94173c
africamonkey <marses@vip.qq.com>
2023-04-16 20:13:16 +0800

    install failure writer.

--- src/common/testing/google_test_main.cpp
+++ src/common/testing/google_test_main.cpp
@@ -1,8 +1,15 @@
+// Copyright (c) 2023. Kaiqi Wang - All Rights Reserved
+
 #include "glog/logging.h"
 #include "gtest/gtest.h"
 
+void FailureWriter(const char *data, size_t size) {
+  fprintf(stderr, "%.*s", (int) size, data);
+}
+
 int main(int argc, char **argv) {
   google::InstallFailureSignalHandler();
+  google::InstallFailureWriter(FailureWriter);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
